f8a6b492d3220e5a770162d6eff31417d6bd33f7
Nick DiPatri <ndipatridev@gmail.com>
2019-03-15 14:50:24 -0400

    no message (+1 squashed commit) Squashed commits: [5198b78] Step 1... welcome intent says hello then ends the conversation.

--- googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
+++ googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
@@ -4,19 +4,8 @@ import com.google.actions.api.ActionRequest
 import com.google.actions.api.ActionResponse
 import com.google.actions.api.DialogflowApp
 import com.google.actions.api.ForIntent
-import okhttp3.MediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
 import org.slf4j.LoggerFactory
-import java.util.logging.Level
-import java.util.logging.Logger
 
-
-/**
- * Implements all intent handlers for this Action. Note that your App must extend from DialogflowApp
- * if using Dialogflow or ActionsSdkApp for ActionsSDK based Actions.
- */
 class MyActionsApp : DialogflowApp() {
 
     @ForIntent("Default Welcome Intent")
@@ -33,39 +22,10 @@ class MyActionsApp : DialogflowApp() {
         } else {
             responseBuilder.add("Hey there!")
         }
-
-        responseBuilder.addSuggestions(arrayOf("Yes", "No"))
+        responseBuilder.add("Ping Pong balls don't hurt even if they hit you on the head.")
 
         LOGGER.info("Welcome intent end.")
-        return responseBuilder.build()
-    }
-
-    private fun actuateServo() {
-        var client = OkHttpClient()
-
-        Logger.getLogger(OkHttpClient::class.java.name).level = Level.FINE
-
-        // GCP has an Integration path for Particle, but we want to demonstrate the use of a
-        // vanilla webhook
-
-        // For our simple webhook, we don't need to send any arguments.
-        // Particle also requires functions to be called via POST
-        var emptyBody = RequestBody.create(MediaType.get("application/json; charset=utf-8"), "")
-
-        var request = Request.Builder()
-                .header("Authorization", "Bearer {access token goes here}")
-                .post(emptyBody)
-                .url("https://api.particle.io/v1/devices/{device id goes here}/actuate")
-
-                .build()
-
-        client.newCall(request).execute().also {
-
-            var respondBody = it.body()!!.source().readUtf8()
-
-            it.close()
-            LOGGER.info("Response: success=${it.isSuccessful}, and response was $respondBody")
-        }
+        return responseBuilder.endConversation().build()
     }
 
     companion object {
