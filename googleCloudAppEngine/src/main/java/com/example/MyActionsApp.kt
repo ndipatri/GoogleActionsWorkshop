c05c9225827947637e330a085a9a79d6ca74d194
Nick DiPatri <ndipatridev@gmail.com>
2019-03-16 14:53:21 -0400

    no message

--- googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
+++ googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
@@ -2,7 +2,6 @@ package com.example
 
 import com.google.actions.api.*
 import com.google.actions.api.response.helperintent.Permission
-import com.google.api.services.actions_fulfillment.v2.model.SpeechResponse
 import org.slf4j.LoggerFactory
 
 
@@ -19,15 +18,19 @@ class MyActionsApp : DialogflowApp() {
             if (userName != null) {
                 // Using Speech Synthesis Markup Language (SSML) for effect:
 
-                add("<speak><p><prosody rate=\"x-slow\" pitch=\"-3st\">Arg</prosody><break time=\"200ms\"/></p></speak>")
+                add("<speak><p><prosody rate=\"x-slow\" pitch=\"-3st\">Arg</prosody>" +
+                    "<break time=\"200ms\"/></p></speak>")
 
-                add(" Ahoy $userName! Would you be loading your catapult now, sir?")
-                addSuggestions(arrayOf("Yes", "No"))
+                add("Ahoy $userName! Would you be loading or launching your " +
+                    "catapult now, sir?")
+                addSuggestions(arrayOf("Load", "Launch"))
             } else {
-                add("ignore this text") // this has to be here or there will be JSON error w/ Dialog Flow
+                add("ignore this text") // this has to be here or there will be
+                                        // JSON error w/ Dialog Flow
                 add(Permission().apply {
                     setPermissions(arrayOf(PERMISSION_NAME))
-                    setContext("Beggin yer pardon, sir! So I don't have to call you sir anymore")
+                    setContext("Beggin yer pardon, sir! " +
+                               "So I don't have to call you sir anymore")
                 })
             }
         }.build().also {
@@ -48,14 +51,29 @@ class MyActionsApp : DialogflowApp() {
 
                 var userName = request.userStorage["userName"] as String
 
-                add("Much obliged $userName, Would you be loading your catapult now, sir?")
-                addSuggestions(arrayOf("Yes", "No"))
+                add("Much obliged $userName, Would you be loading or " +
+                    " launching your catapult now, sir?")
+                addSuggestions(arrayOf("Load", "Launch"))
             } else {
-                add("I respect a Pirate's Privacy! Would you be loading your catapult now, sir?")
-                addSuggestions(arrayOf("Yes", "No"))
+                add("I respect a Pirate's Privacy! Would you be loading " +
+                    "or launching your catapult now, sir?")
+                addSuggestions(arrayOf("Load", "Launch"))
             }
         }.build().also {
             LOGGER.info("Permissions intent end.")
+        }
+    }
+
+    @ForIntent("Default Welcome Intent - custom")
+    fun welcomeFollowUp(request: ActionRequest): ActionResponse {
+        LOGGER.info("Welcome follow-up intent started.")
+
+        return getResponseBuilder(request).apply {
+
+            add("Command requested is ${request.getParameter("command")}.")
+
+        }.build().also {
+            LOGGER.info("Welcome follow-up intent ended.")
         }
     }
 
