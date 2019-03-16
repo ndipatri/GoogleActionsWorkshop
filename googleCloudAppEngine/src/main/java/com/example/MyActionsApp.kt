265ba520057880e3033f96ffca6f1d680392261d
Nick DiPatri <ndipatridev@gmail.com>
2019-03-16 14:55:48 -0400

    Step 3 - webhook to actuate Particle Phone-based catapult.

--- googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
+++ googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
@@ -2,7 +2,13 @@ package com.example
 
 import com.google.actions.api.*
 import com.google.actions.api.response.helperintent.Permission
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
 import org.slf4j.LoggerFactory
+import java.util.logging.Level
+import java.util.logging.Logger
 
 
 class MyActionsApp : DialogflowApp() {
@@ -72,8 +78,38 @@ class MyActionsApp : DialogflowApp() {
 
             add("Command requested is ${request.getParameter("command")}.")
 
+            // TODO fill this out for both Load and Launch cases (include catapult instructions)
+
         }.build().also {
             LOGGER.info("Welcome follow-up intent ended.")
+        }
+    }
+
+    private fun actuateServo() {
+        var client = OkHttpClient()
+
+        Logger.getLogger(OkHttpClient::class.java.name).level = Level.FINE
+
+        // GCP has an Integration path for Particle, but we want to demonstrate the use of a
+        // vanilla webhook
+
+        // For our simple webhook, we don't need to send any arguments.
+        // Particle also requires functions to be called via POST
+        var emptyBody = RequestBody.create(MediaType.get("application/json; charset=utf-8"), "")
+
+        var request = Request.Builder()
+                .header("Authorization", "Bearer {access token goes here}")
+                .post(emptyBody)
+                .url("https://api.particle.io/v1/devices/{device id goes here}/actuate")
+
+                .build()
+
+        client.newCall(request).execute().also {
+
+            var respondBody = it.body()!!.source().readUtf8()
+
+            it.close()
+            LOGGER.info("Response: success=${it.isSuccessful}, and response was $respondBody")
         }
     }
 
