c1ff6245584e4d3c1106ab8c611d99b1c659801e
Nick DiPatri <ndipatridev@gmail.com>
2019-03-16 10:15:04 -0400

    Step 1... welcome intent says hello then ends the

--- googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
+++ googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
@@ -1,35 +1,36 @@
 package com.example
 
-import com.google.actions.api.ActionRequest
-import com.google.actions.api.ActionResponse
-import com.google.actions.api.DialogflowApp
-import com.google.actions.api.ForIntent
+import com.google.actions.api.*
+import com.google.actions.api.response.helperintent.Permission
 import org.slf4j.LoggerFactory
 
 class MyActionsApp : DialogflowApp() {
 
     @ForIntent("Default Welcome Intent")
     fun welcome(request: ActionRequest): ActionResponse {
-        LOGGER.info("Welcome intent start.")
 
-        val responseBuilder = getResponseBuilder(request)
+        return getResponseBuilder(request).apply {
+            LOGGER.info("Welcome intent started.")
 
-        // Notice use of built-in User object to help with building a
-        // customized experience.
-        val user = request.user
+            // Notice use of built-in User object to help with building a
+            // customized experience.
+            val user = request.user
 
-        if (user != null && user!!.getLastSeen() != null) {
-            responseBuilder.add("Hey there! Good to see you again!")
-        } else {
-            responseBuilder.add("Hey there!")
+            if (user?.lastSeen != null) {
+                add("Hey there! Good to see you again!")
+            } else {
+                add("Hey there!")
+            }
+            add("Ping Pong balls don't hurt even if they hit you on the head.")
+
+            endConversation()
+
+        }.build().also {
+            LOGGER.info("Welcome intent ended.")
         }
-        responseBuilder.add("Ping Pong balls don't hurt even if they hit you on the head.")
-
-        LOGGER.info("Welcome intent end.")
-        return responseBuilder.endConversation().build()
     }
 
     companion object {
-        private val LOGGER = LoggerFactory.getLogger(MyActionsApp::class.java!!)
+        private val LOGGER = LoggerFactory.getLogger(MyActionsApp::class.java)
     }
 }
