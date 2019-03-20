220f54ee3bc845f58d579b1f23b56b7af6b397ff
Nick DiPatri <ndipatridev@gmail.com>
2019-03-20 05:27:14 -0400

    no message

--- googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
+++ googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
@@ -78,6 +78,46 @@ class MyActionsApp : DialogflowApp() {
         }
     }
 
+    @ForIntent("Load Catapult Follow-Up")
+    fun loadCatapult(request: ActionRequest): ActionResponse {
+        LOGGER.info("'Load Catapult Follow-Up' intent started.")
+
+        return getResponseBuilder(request).apply {
+
+            add(StringBuilder().apply {
+                append("<speak>")
+                append(
+                        if (request.getParameter("confusion").toString().isNotEmpty()) {
+                            "Sorry, I'm going a bit too fast it seems!  Go get the catapult " +
+                            "and a ping ping ball and prepare to pull the arm " +
+                            "all the way back"
+                        } else {
+                            "Got it! Prepare to load the catapult! "
+                        }
+                )
+
+                append("Put a Ping Pong ball in the basket and get ready to pull the " +
+                        "launch arm all the way back. When you are ready say 'Load'!")
+
+                append("</speak>")
+            }.toString())
+
+
+
+            // actuateServo()
+
+
+            add("<speak>" +
+                    "Catapult is loaded! Aim the catapult and when " +
+                    "you're ready say 'Fire!' to launch the " +
+                    "ping pong ball!" +
+                    "</speak>")
+
+        }.build().also {
+            LOGGER.info("'Load Catapult Follow-Up' intent ended.")
+        }
+    }
+
 
 
 
