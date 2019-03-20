1a6df715be6ac95636dc62148fae11d907da3fa1
Nick DiPatri <ndipatridev@gmail.com>
2019-03-20 10:16:53 -0400

    no message

--- googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
+++ googleCloudAppEngine/src/main/java/com/example/MyActionsApp.kt
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory
 import java.util.logging.Level
 import java.util.logging.Logger
 
-
 class MyActionsApp : DialogflowApp() {
 
     @ForIntent("Default Welcome Intent")
@@ -25,16 +24,16 @@ class MyActionsApp : DialogflowApp() {
                 // Using Speech Synthesis Markup Language (SSML) for effect:
 
                 add("<speak><p><prosody rate=\"x-slow\" pitch=\"-3st\">Arg</prosody>" +
-                    "<break time=\"200ms\"/></p></speak>")
+                        "<break time=\"200ms\"/></p></speak>")
 
                 add("Ahoy $userName! Let me know when you are ready!")
             } else {
                 add("ignore this text") // this has to be here or there will be
-                                        // JSON error w/ Dialog Flow
+                // JSON error w/ Dialog Flow
                 add(Permission().apply {
                     setPermissions(arrayOf(PERMISSION_NAME))
                     setContext("Beggin yer pardon, sir! " +
-                               "So I don't have to call you sir anymore")
+                            "So I don't have to call you sir anymore")
                 })
             }
         }.build().also {
@@ -56,11 +55,11 @@ class MyActionsApp : DialogflowApp() {
                 var userName = request.userStorage["userName"] as String
 
                 add("Much obliged $userName, Let me know when you " +
-                    "are ready!")
+                        "are ready!")
 
             } else {
                 add("I respect a Pirate's privacy! Let me know when " +
-                    "you are ready!")
+                        "you are ready!")
 
             }
         }.build().also {
@@ -81,10 +80,10 @@ class MyActionsApp : DialogflowApp() {
 
                 if (request.getParameter("confusion").toString().isNotEmpty()) {
                     append("Sorry, I'm going a bit too fast it seems!  We're going to " +
-                           "launch a ping pong ball using a popsicle stick and rubber " +
-                           "band catapult. Go get the ping pong ball and the " +
-                           "catapult. Put the ball in the basket and get ready to pull " +
-                           "the launch arm all the way back. ")
+                            "launch a ping pong ball using a popsicle stick and rubber " +
+                            "band catapult. Go get the ping pong ball and the " +
+                            "catapult. Put the ball in the basket and get ready to pull " +
+                            "the launch arm all the way back. ")
                 }
 
                 append("When you are ready say 'Load'!")
@@ -103,13 +102,13 @@ class MyActionsApp : DialogflowApp() {
 
         return getResponseBuilder(request).apply {
 
-            //actuateServo()
+            actuateServo()
 
             add("<speak>" +
                     "Catapult is loaded! Aim the catapult and when " +
                     "you're ready say 'Fire!' to launch the " +
                     "ping pong ball!" +
-                "</speak>")
+                    "</speak>")
 
         }.build().also {
             LOGGER.info("'Load Catapult Follow-Up' intent ended.")
@@ -122,7 +121,7 @@ class MyActionsApp : DialogflowApp() {
 
         return getResponseBuilder(request).apply {
 
-            //actuateServo()
+            actuateServo()
 
             add("<speak>" +
                     "Great shot! Ready to start again or do you want to quit?" +
